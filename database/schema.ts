11c59eefde815adb7787be9d66e9fc0690e82ef1
Divyansh Saxena <divyanshsxn998@gmail.com>
2024-04-13 15:39:53 +0530

    db relations

--- database/schema.ts
+++ database/schema.ts
@@ -1,7 +1,32 @@
-import { pgTable, serial, text } from "drizzle-orm/pg-core";
+import { relations } from "drizzle-orm";
+import { integer, pgTable, serial, text } from "drizzle-orm/pg-core";
 
 export const courses = pgTable("courses", {
   id: serial("id").primaryKey(),
   title: text("title").notNull(),
   imageSrc: text("image_src").notNull(),
 });
+
+// many to many relationship of userProgress and courses
+export const courseRelations = relations(courses, ({ many }) => ({
+  userProgress: many(userProgress),
+}));
+
+export const userProgress = pgTable("user_progress", {
+  userId: text("user_id").primaryKey(),
+  userName: text("user_name").notNull().default("User"),
+  userImageSrc: text("user_image_src").notNull().default("next.svg"),
+  activeCourseId: integer("active_course_id").references(() => courses.id, {
+    onDelete: "cascade",
+  }),
+  hearts: integer("hearts").notNull().default(0),
+  points: integer("points").notNull().default(0),
+});
+
+// one to many relationship of userProgress and activeCourse
+export const userProgressRelation = relations(userProgress, ({ one }) => ({
+  activeCourse: one(courses, {
+    fields: [userProgress.activeCourseId],
+    references: [courses.id],
+  }),
+}));
