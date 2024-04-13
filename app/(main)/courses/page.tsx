8465a7c6a2dbd4481b4f649a7c46ee9b3fdb5f20
Divyansh Saxena <divyanshsxn998@gmail.com>
2024-04-13 15:54:33 +0530

    getUserProgress

--- app/(main)/courses/page.tsx
+++ app/(main)/courses/page.tsx
@@ -1,14 +1,21 @@
-import { getCourses } from "@/database/queries";
+import { getCourses, getUserProgress } from "@/database/queries";
 import { List } from "./List";
 
-const Courses = async () => {
-  const courses = await getCourses();
+const CoursesPage = async () => {
+  const coursesData = getCourses();
+  const userProgressData = getUserProgress();
+
+  const [courses, userProgress] = await Promise.all([
+    coursesData,
+    userProgressData,
+  ]);
+
   return (
     <div className="h-full max-w-[912px] px-3 m-auto">
       <h1 className="text-2xl font-bold to-neutral-700">Language Courses</h1>
-      <List courses={courses} activeCourseId={1} />
+      <List courses={courses} activeCourseId={userProgress?.activeCourseId} />
     </div>
   );
 };
 
-export default Courses;
+export default CoursesPage;
