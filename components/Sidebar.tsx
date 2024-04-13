3065d266d4008854c57c2e93d79db81d7b0b68ae
Divyansh Saxena <divyanshsxn998@gmail.com>
2024-04-13 10:11:52 +0530

    sidebar

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -1,4 +1,9 @@
 import { cn } from "@/lib/utils";
+import Image from "next/image";
+import Link from "next/link";
+import SidebarItem from "./SidebarItem";
+import { ClerkLoaded, ClerkLoading, UserButton } from "@clerk/nextjs";
+import { Loader } from "lucide-react";
 
 type Props = {
   className?: string;
@@ -8,11 +13,36 @@ const Sidebar = ({ className }: Props) => {
   return (
     <div
       className={cn(
-        "flex bg-blue-500 h-full lg:w-[256px] lg:fixed left-0 top-0 px-4 border-r-2 flex-col",
+        "flex h-full lg:w-[256px] lg:fixed left-0 top-0 px-4 border-r-2 flex-col",
         className
       )}
     >
-      Sidebar
+      <Link href="/learn">
+        <div className="pt-8 pl-4 pb-7 flex items-center gap-x-3">
+          <Image src="next.svg" height={40} width={40} alt="mascot" />
+          <h1 className="text-2xl font-extrabold text-green-600 tracking-wide">
+            Lingo
+          </h1>
+        </div>
+      </Link>
+      <div className="flex flex-col gap-y-2 flex-1">
+        <SidebarItem label="Learn" href="/learn" iconSrc="next.svg" />
+        <SidebarItem
+          label="Leaderboard"
+          href="/leaderboard"
+          iconSrc="next.svg"
+        />
+        <SidebarItem label="Quests" href="/quests" iconSrc="next.svg" />
+        <SidebarItem label="Shop" href="/shop" iconSrc="next.svg" />
+      </div>
+      <div className="p-4">
+        <ClerkLoading>
+          <Loader className="h-5 w-5 text-black animate-spin" />
+        </ClerkLoading>
+        <ClerkLoaded>
+          <UserButton afterSignOutUrl="/" />
+        </ClerkLoaded>
+      </div>
     </div>
   );
 };
---
+++ components/SidebarItem.tsx
@@ -0,0 +1,37 @@
+"use client";
+
+import { usePathname } from "next/navigation";
+import { Button } from "./ui/button";
+import Link from "next/link";
+import Image from "next/image";
+
+type Props = {
+  label: string;
+  iconSrc: string;
+  href: string;
+};
+
+const SidebarItem = ({ label, iconSrc, href }: Props) => {
+  const pathname = usePathname();
+  const isActive = pathname === href;
+  return (
+    <Button
+      variant={isActive ? "sidebarOutline" : "sidebar"}
+      className="justify-start h-[52px]"
+      asChild
+    >
+      <Link href="/learn">
+        <Image
+          src={iconSrc}
+          height={32}
+          width={32}
+          alt={label}
+          className="mr-5"
+        />
+        {label}
+      </Link>
+    </Button>
+  );
+};
+
+export default SidebarItem;
